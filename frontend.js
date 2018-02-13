7ec5c6caba29a6776def0406cc4352ba9c6fe094
jift77 <serviciotecnicotoyota@gmail.com>
2018-02-13 00:09:15 -0500

    Reading file functionality added

--- frontend.js
+++ frontend.js
@@ -1,23 +1,72 @@
-function registro(codigo, producto)
+import fs from 'fs'
+
+// Clase producto y los atribtos de la clase
+function producto(codigo, nombre)
 {
     this.codigo = codigo;
-    this.producto = producto;
+    this.nombre = nombre;
+
+    this.ConvetirmeEnPlano = function(){
+        return `${this.codigo},${this.nombre}\n`
+    }
 }
 
+// model es para manipular mas facil los datos entre javascript y html
 var model  = function ()
 {
+    // variables q contienen los valores que se ingresan en pantalla 
     this.codigo = ko.observable("")
-    this.producto = ko.observable("")
+    this.nombre = ko.observable("")
 
-    this.registros = ko.observableArray([
-        new registro('1','televisor'),
-        new registro('2','radio')
+    this.productos = ko.observableArray([
+        //new producto('1','televisor'),
+        //new producto('2','radio')
     ])
 
     this.add = function () {
-        this.registros.push(new registro(this.codigo(), this.producto()))
-        //console.log(self)
+        //this.productos.push(new producto(this.codigo(), this.nombre()))
+        alert('algo')
+        console.log(self)
+        let productoAInsertar = new producto(this.codigo(), this.nombre());
+        fs.appendFileSync('tabla.txt',productoAInsertar.ConvetirmeEnPlano())
+        fs.readFile('tabla.txt','utf-8',(err,data)=> {
+            this.productos.removeAll();
+            let array = data.toString().split("\n");
+            let la = array.length;
+            for(let i = 0; i < la; i++) 
+            {
+                let registro = array[i].split(',');
+                this.productos.push(new producto(registro[0],registro[1]));
+            }
+        })
     }.bind(this)
+
+    this.leerArchivo = function()
+    {
+        fs.readFile('tabla.txt','utf-8',(err,data)=> {
+            this.productos.removeAll();
+            let array = data.toString().split("\n");
+            let la = array.length;
+            for(let i = 0; i < la; i++) 
+            {
+                let registro = array[i].split(',');
+                this.productos.push(new producto(registro[0],registro[1]));
+            }
+        })
+    }.bind(this)
+
+    function actualizarInterfaz() {
+        fs.readFile('tabla.txt','utf-8',(err,data)=> {
+            this.productos.removeAll();
+            let array = data.toString().split("\n");
+            let la = array.length;
+            for(let i = 0; i < la; i++) 
+            {
+                let registro = array[i].split(',');
+                this.productos.push(new producto(registro[0],registro[1]));
+            }
+        })
+    } 
 }
 
 $(document).ready(() => {
